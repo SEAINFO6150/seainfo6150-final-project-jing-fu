f9ed8a5afc1bfbb6cd6b7c4092f4de7ddd69d6d1
monicafu <31578677+monicafu@users.noreply.github.com>
2020-04-09 17:39:43 -0700

    Fixed feedback of user testing from April

--- src/Filter/Filter.jsx
+++ src/Filter/Filter.jsx
@@ -8,7 +8,7 @@ const Filter = ({onChange}) => {
     return (
       <div className={styles.filter}>
         <select className={styles.sortBy} onChange={onChange}> 
-          <option value="asc">Sort By: </option>
+          <option value="asc">Sort By </option>
           <option value="asc">Sort By: Name A to Z</option>
           <option value="desc">Sort By: Name Z to A</option>
           <option value="lowToHigh">Sort By: Price Low to High</option>
